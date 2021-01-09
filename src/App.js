9ac93a413c200b3f768b11431553b140a8f96a55
Smayan Joshi <joshismayan@gmail.com>
2021-01-09 15:16:49 +0530

    Style Modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import Board from "./Board";
 
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(prop => ({
+  header: {
+    textAlign: "center",
+    padding: "10px",
+    backgroundColor: "green"
+  },
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "10px"
+  },
+  boardContainer: { width: "70vh", height: "70vh" }
+}));
+
 function App() {
+  const classes = useStyles();
   const [size, setSize] = useState(3);
   return (
     <>
-      <div
-        style={{
-          textAlign: "center",
-          padding: "10px",
-          backgroundColor: "green"
-        }}
-      >
+      <div className={classes.header}>
         <h1>Tic Tac Toe</h1>
         <span>Size </span>
         <input
@@ -21,15 +33,8 @@ function App() {
           onChange={event => setSize(parseInt(event.target.value))}
         />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "10px"
-        }}
-      >
-        <div style={{ width: "70vh", height: "70vh" }}>
+      <div className={classes.content}>
+        <div className={classes.boardContainer}>
           <Board size={size} />
         </div>
       </div>
